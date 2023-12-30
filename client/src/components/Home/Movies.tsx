84acd9362d2fa249e628d399df9ea970df66bf3a
nader2356 <naderabdessaied8@gmail.com>
2023-12-30 19:47:44 +0100

    add a spinner when refetching

--- client/src/components/Home/Movies.tsx
+++ client/src/components/Home/Movies.tsx
@@ -1,88 +1,88 @@
+
+import { useEffect } from "react";
 import {
-    Spinner,
-    Text,
-    Grid,
-    Heading,
-    Box,
-    Divider,
-    Flex,
-  } from "@chakra-ui/react";
-  import { useQuery } from "@tanstack/react-query";
-  import { useParams } from "react-router-dom";
-  import MovieCard from "./MovieCard";
-  
+  Spinner,
+  Text,
+  Grid,
+  Heading,
+  Box,
+  Divider,
+  Flex,
+} from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import MovieCard from "./MovieCard";
 
-  import { AnimatePresence, motion } from "framer-motion";
-  import { useEffect } from "react";
-import { IMovie } from "../../utils/interfaces";
 import { getAllMovies } from "../../api/movies";
-  
-  const Movies = () => {
-    const genre = useParams().genre;
+import { AnimatePresence, motion } from "framer-motion";
 
-  
 
-    const {
-      data: movies,
-      isSuccess,
-      isLoading,
-      isError,
-      refetch,
-    } = useQuery({
-      queryKey: ["movies"],
-      queryFn: () => getAllMovies(genre),
-    });
+const Movies = () => {
+  const genre = useParams().genre;
+  const {
+    data: movies,
+    isSuccess,
+    isLoading,
+    isRefetching,
+    isError,
+    refetch,
+  } = useQuery({
+    queryKey: ["movies"],
+    queryFn: () => getAllMovies(genre),
+  });
+  useEffect(() => {
+    refetch();
+  }, [genre]);
 
-    useEffect(() => {
-        refetch();
-      }, [genre]);
-
-    return (
-      <Box mb="2rem" maxW="90rem" mx="auto" px="2rem">
+  return (
+    <Box mb="2rem" maxW="90rem" mx="auto" px="2rem">
+    
+      <Flex justify="space-between" align="center">
         <Heading as="h2" size="lg">
           Movies
         </Heading>
-        <Divider my="1rem" />
-        {isLoading ? (
-          <Flex w="full" justify="center">
-            <Spinner size="xl" />
-          </Flex>
-        ) : null}
-        {isError ? <Text mx="auto">failed</Text> : null}
-   
-        {isSuccess ? (
-          <Grid
-            templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
-            gridAutoRows="auto"
-            w="full"
-            rowGap="3rem"
-            columnGap="1rem"
-          >
-            <AnimatePresence>
-              {movies.length > 0 ? (
-                <>
-                  {movies.map((movie: IMovie) => (
-                    <MovieCard key={movie.id} movie={movie} />
-                  ))}
-                </>
-              ) : (
-                <Text
-                  as={motion.p}
-                  initial={{ scale: 0 }}
-                  animate={{
-                    scale: 1,
-                    transition: { delay: 0.2, duration: 0.3 },
-                  }}
-                  textAlign="center"
-                >
-                  NO MOVIES FOUND
-                </Text>
-              )}
-            </AnimatePresence>
-          </Grid>
-        ) : null}
-        <Divider as={motion.hr} layout my="1rem" />
-      </Box>
-    );
-  };
-  export default Movies;
+        {isRefetching ? <Spinner size="sm" /> : null}
+      </Flex>
+      <Divider my="1rem" />
+      {isLoading ? (
+        <Flex w="full" justify="center">
+          <Spinner size="xl" />
+        </Flex>
+      ) : null}
+      {isError ? <Text mx="auto">failed</Text> : null}
+      {isSuccess ? (
+        <Grid
+          templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+          gridAutoRows="auto"
+          w="full"
+          rowGap="3rem"
+          columnGap="1rem"
+        >
+          <AnimatePresence>
+            {movies.length > 0 ? (
+              <>
+                {movies.map((movie) => (
+                  <MovieCard key={movie.id} movie={movie} />
+                ))}
+              </>
+            ) : (
+              <Text
+                as={motion.p}
+                initial={{ scale: 0 }}
+                animate={{
+                  scale: 1,
+                  transition: { delay: 0.2, duration: 0.3 },
+                }}
+                textAlign="center"
+              >
+                NO MOVIES FOUND
+              </Text>
+            )}
+          </AnimatePresence>
+        </Grid>
+      ) : null}
+      <Divider as={motion.hr} layout my="1rem" />
+    </Box>
+  );
+};
+export default Movies;
