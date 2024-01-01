63d7f4f02459b45fa5c25b00035b4a6064fdc961
nader2356 <naderabdessaied8@gmail.com>
2024-01-01 12:59:04 +0100

    add a loading spinner while fetching main app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter as Router } from "react-router-dom";
+import axios from "axios";
+
+
+const App = lazy(() => import("./App"));
+import theme from "./theme";
+import FullPageSpinner from "./components/FullPageSpinner";
+
+const queryClient = new QueryClient();
+
+axios.defaults.baseURL = "/api";
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <App />
+          <Suspense fallback={<FullPageSpinner />}>
+            <App />
+          </Suspense>
+        </Router>
+      </QueryClientProvider>
+    </ChakraProvider>
+  </React.StrictMode>
+);
